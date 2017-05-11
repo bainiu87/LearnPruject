3f1397769b93c9cd2b09761acd396470e0e8ffe9
baiyun <baiyun5522359@163.com>
2017-05-12 00:21:55 +0800

    1.5

--- J.D/mysql/CmdHandle.py
+++ J.D/mysql/CmdHandle.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import sys
+import datetime
 class CmdHandle(object):
 
     def __init__(self):
@@ -16,9 +17,38 @@ class CmdHandle(object):
             subprocess.check_call(cmd,shell=True)
             filename += 1
 
+    def produceDayAction(self,begin,over):
+        sql = "select user_id,sku_id,time,model_id,type,cate,brand from action_201602 where '%s'<=time and time<'%s'"%(begin,over)
+        cmd = 'mysql -uroot -proot -Bse "set names utf8;use dj;%s;">%s\\smple\\day_%s.txt' % (sql, self.fileDir,str(begin)[:10])
+        subprocess.check_call(cmd, shell=True)
+
+    def loadDayToMysql(self,begin):
+        sql = "load data local infile '%s\\smple\\day_%s.txt' into table day_%s FIELDS ESCAPED BY '\\' TERMINATED BY '\t' ENCLOSED BY '%s'LINES TERMINATED BY '\n' ( user_id , sku_id ,time, model_id ,type , cate , brand )"%(self.fileDir,str(begin)[:10],str(begin)[:10].replace('-','_'),'"')
+        cmd = 'mysql -uroot -proot -Bse "set names utf8;use dj;%s;"'%(sql)
+        print cmd
+        subprocess.check_call(cmd, shell=True)
+
+    def createTable(self,begin):
+        sql = "create table day_%s(id int not null auto_increment,user_id int,sku_id int,time datetime,model_id varchar(20),type int,cate int,brand int,primary key (id))"%(str(begin)[:10].replace('-','_'))
+        cmd = 'mysql -uroot -proot -Bse "set names utf8;use dj;%s;"' % (sql)
+        subprocess.check_call(cmd, shell=True)
+
+    def timeToTime(self):
+        begin = datetime.datetime(2016,01,31,00,00,00)
+        over = begin + datetime.timedelta(days=1)
+        state = True
+        while state:
+            self.createTable(begin=begin)
+            self.produceDayAction(begin=begin,over=over)
+            self.loadDayToMysql(begin=begin)
+            if begin == datetime.datetime(2016,03,01,00,00,00):
+                state = False
+
     # 联表查询商品、用户、评论详情
     def getInfo(self):
         sql = ""
 if __name__=='__main__':
     cmdObject = CmdHandle()
-    cmdObject.getLabel()
+    # cmdObject.produceDayAction()
+    # cmdObject.loadDayToMysql()
+    cmdObject.timeToTime()
